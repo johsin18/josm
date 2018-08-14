johsin18/josm

commit 46672d716050ea1123e03a4c22109485ab5e9235

add `AbstractOsmDataLayer`, `MainLayerManager.getActiveData`, `Main.getInProgressISelection`
git-svn-id: https://josm.openstreetmap.de/svn/trunk@13926 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/actions/mapmode/SelectAction.java
@@ -34,6 +34,7 @@ import org.openstreetmap.josm.data.coor.EastNorth;
 import org.openstreetmap.josm.data.coor.LatLon;
 import org.openstreetmap.josm.data.osm.DataSet;
 import org.openstreetmap.josm.data.osm.Node;
+import org.openstreetmap.josm.data.osm.OsmData;
 import org.openstreetmap.josm.data.osm.OsmPrimitive;
 import org.openstreetmap.josm.data.osm.Way;
 import org.openstreetmap.josm.data.osm.WaySegment;
@@ -353,7 +354,7 @@ public class SelectAction extends MapMode implements ModifierExListener, KeyPres
      */
     private boolean removeHighlighting() {
         boolean needsRepaint = false;
-        DataSet ds = getLayerManager().getActiveDataSet();
+        OsmData<?, ?, ?, ?> ds = getLayerManager().getActiveData();
         if (ds != null && !ds.getHighlightedVirtualNodes().isEmpty()) {
             needsRepaint = true;
             ds.clearHighlightedVirtualNodes();

src/org/openstreetmap/josm/gui/layer/MainLayerManager.java
@@ -13,6 +13,7 @@ import java.util.concurrent.CopyOnWriteArrayList;
 import javax.swing.JOptionPane;
 
 import org.openstreetmap.josm.data.osm.DataSet;
+import org.openstreetmap.josm.data.osm.OsmData;
 import org.openstreetmap.josm.gui.MainApplication;
 import org.openstreetmap.josm.gui.io.AsynchronousUploadPrimitivesTask;
 import org.openstreetmap.josm.gui.util.GuiHelper;
@@ -174,7 +175,12 @@ public class MainLayerManager extends LayerManager {
     /**
      * The current active data layer. It might be editable or not, based on its read-only status.
      */
-    private OsmDataLayer dataLayer;
+    private AbstractOsmDataLayer dataLayer;
+
+    /**
+     * The current active OSM data layer. It might be editable or not, based on its read-only status.
+     */
+    private OsmDataLayer osmDataLayer;
 
     private final List<ActiveLayerChangeListener> activeLayerChangeListeners = new CopyOnWriteArrayList<>();
     private final List<LayerAvailabilityListener> layerAvailabilityListeners = new CopyOnWriteArrayList<>();
@@ -261,19 +267,22 @@ public class MainLayerManager extends LayerManager {
     }
 
     private void setActiveLayer(Layer layer, boolean forceEditLayerUpdate) {
-        ActiveLayerChangeEvent event = new ActiveLayerChangeEvent(this, dataLayer, activeLayer);
+        ActiveLayerChangeEvent event = new ActiveLayerChangeEvent(this, osmDataLayer, activeLayer);
         activeLayer = layer;
+        if (activeLayer instanceof AbstractOsmDataLayer) {
+            dataLayer = (AbstractOsmDataLayer) layer;
+        }
         if (activeLayer instanceof OsmDataLayer) {
-            dataLayer = (OsmDataLayer) activeLayer;
+            osmDataLayer = (OsmDataLayer) activeLayer;
         } else if (forceEditLayerUpdate) {
-            dataLayer = null;
+            osmDataLayer = null;
         }
         fireActiveLayerChange(event);
     }
 
     private void fireActiveLayerChange(ActiveLayerChangeEvent event) {
         GuiHelper.assertCallFromEdt();
-        if (event.getPreviousActiveLayer() != activeLayer || event.getPreviousDataLayer() != dataLayer) {
+        if (event.getPreviousActiveLayer() != activeLayer || event.getPreviousDataLayer() != osmDataLayer) {
             for (ActiveLayerChangeListener l : activeLayerChangeListeners) {
                 l.activeOrEditLayerChanged(event);
             }
@@ -306,7 +315,7 @@ public class MainLayerManager extends LayerManager {
             return new ArrayList<>();
         }
 
-        if (layer == activeLayer || layer == dataLayer) {
+        if (layer == activeLayer || layer == osmDataLayer) {
             Layer nextActive = suggestNextActiveLayer(layer);
             setActiveLayer(nextActive, true);
         }
@@ -375,8 +384,8 @@ public class MainLayerManager extends LayerManager {
      * @see #getActiveDataLayer
      */
     public synchronized OsmDataLayer getEditLayer() {
-        if (dataLayer != null && !dataLayer.isLocked())
-            return dataLayer;
+        if (osmDataLayer != null && !osmDataLayer.isLocked())
+            return osmDataLayer;
         else
             return null;
     }
@@ -389,8 +398,8 @@ public class MainLayerManager extends LayerManager {
      * @since 13434
      */
     public synchronized OsmDataLayer getActiveDataLayer() {
-        if (dataLayer != null)
-            return dataLayer;
+        if (osmDataLayer != null)
+            return osmDataLayer;
         else
             return null;
     }
@@ -401,8 +410,8 @@ public class MainLayerManager extends LayerManager {
      * @see #getActiveDataSet
      */
     public synchronized DataSet getEditDataSet() {
-        if (dataLayer != null && !dataLayer.isLocked()) {
-            return dataLayer.getDataSet();
+        if (osmDataLayer != null && !osmDataLayer.isLocked()) {
+            return osmDataLayer.getDataSet();
         } else {
             return null;
         }
@@ -411,12 +420,25 @@ public class MainLayerManager extends LayerManager {
     /**
      * Gets the data set of the active data layer. The dataset can be read-only.
      * @return That data set, <code>null</code> if there is no active data layer.
+     * @since 13926
+     */
+    public synchronized OsmData<?, ?, ?, ?> getActiveData() {
+        if (dataLayer != null) {
+            return dataLayer.getDataSet();
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Gets the data set of the active {@link OsmDataLayer}. The dataset can be read-only.
+     * @return That data set, <code>null</code> if there is no active data layer.
      * @see #getEditDataSet
      * @since 13434
      */
     public synchronized DataSet getActiveDataSet() {
-        if (dataLayer != null) {
-            return dataLayer.getDataSet();
+        if (osmDataLayer != null) {
+            return osmDataLayer.getDataSet();
         } else {
             return null;
         }
@@ -474,8 +496,8 @@ public class MainLayerManager extends LayerManager {
      * @since 13150
      */
     public void invalidateEditLayer() {
-        if (dataLayer != null) {
-            dataLayer.invalidate();
+        if (osmDataLayer != null) {
+            osmDataLayer.invalidate();
         }
     }
 
@@ -511,9 +533,9 @@ public class MainLayerManager extends LayerManager {
         layer.lock();
 
         // Reset only the edit layer as empty
-        if (dataLayer == layer) {
-            ActiveLayerChangeEvent activeLayerChangeEvent = new ActiveLayerChangeEvent(this, dataLayer, activeLayer);
-            dataLayer = null;
+        if (osmDataLayer == layer) {
+            ActiveLayerChangeEvent activeLayerChangeEvent = new ActiveLayerChangeEvent(this, osmDataLayer, activeLayer);
+            osmDataLayer = null;
             fireActiveLayerChange(activeLayerChangeEvent);
         }
     }
@@ -531,9 +553,9 @@ public class MainLayerManager extends LayerManager {
         layer.unsetUploadInProgress();
 
         // Set the layer as edit layer if the edit layer is empty.
-        if (dataLayer == null) {
-            ActiveLayerChangeEvent layerChangeEvent = new ActiveLayerChangeEvent(this, dataLayer, activeLayer);
-            dataLayer = layer;
+        if (osmDataLayer == null) {
+            ActiveLayerChangeEvent layerChangeEvent = new ActiveLayerChangeEvent(this, osmDataLayer, activeLayer);
+            osmDataLayer = layer;
             fireActiveLayerChange(layerChangeEvent);
         }
     }

src/org/openstreetmap/josm/Main.java
@@ -31,6 +31,8 @@ import org.openstreetmap.josm.data.coor.conversion.CoordinateFormatManager;
 import org.openstreetmap.josm.data.coor.conversion.DecimalDegreesCoordinateFormat;
 import org.openstreetmap.josm.data.coor.conversion.ICoordinateFormat;
 import org.openstreetmap.josm.data.osm.DataSet;
+import org.openstreetmap.josm.data.osm.IPrimitive;
+import org.openstreetmap.josm.data.osm.OsmData;
 import org.openstreetmap.josm.data.osm.OsmPrimitive;
 import org.openstreetmap.josm.data.preferences.JosmBaseDirectories;
 import org.openstreetmap.josm.data.projection.Projection;
@@ -264,12 +266,22 @@ public abstract class Main {
     }
 
     /**
-     * Replies the current selected primitives, from a end-user point of view.
+     * Replies the current selected OSM primitives, from a end-user point of view.
      * It is not always technically the same collection of primitives than {@link DataSet#getSelected()}.
-     * @return The current selected primitives, from a end-user point of view. Can be {@code null}.
+     * @return The current selected OSM primitives, from a end-user point of view. Can be {@code null}.
      * @since 6546
      */
     public Collection<OsmPrimitive> getInProgressSelection() {
+        return Collections.emptyList();
+    }
+
+    /**
+     * Replies the current selected primitives, from a end-user point of view.
+     * It is not always technically the same collection of primitives than {@link OsmData#getSelected()}.
+     * @return The current selected primitives, from a end-user point of view. Can be {@code null}.
+     * @since 13926
+     */
+    public Collection<? extends IPrimitive> getInProgressISelection() {
         return Collections.emptyList();
     }
 

src/org/openstreetmap/josm/actions/AbstractInfoAction.java
@@ -15,6 +15,7 @@ import org.openstreetmap.josm.Main;
 import org.openstreetmap.josm.data.notes.Note;
 import org.openstreetmap.josm.data.osm.AbstractPrimitive;
 import org.openstreetmap.josm.data.osm.DataSet;
+import org.openstreetmap.josm.data.osm.OsmData;
 import org.openstreetmap.josm.data.osm.OsmPrimitive;
 import org.openstreetmap.josm.gui.HelpAwareOptionPane;
 import org.openstreetmap.josm.gui.HelpAwareOptionPane.ButtonSpec;
@@ -154,7 +155,7 @@ public abstract class AbstractInfoAction extends JosmAction {
 
     @Override
     protected void updateEnabledState() {
-        DataSet ds = getLayerManager().getActiveDataSet();
+        OsmData<?, ?, ?, ?> ds = getLayerManager().getActiveData();
         setEnabled(ds != null && !ds.selectionEmpty());
     }
 

src/org/openstreetmap/josm/actions/AutoScaleAction.java
@@ -25,6 +25,8 @@ import org.openstreetmap.josm.data.Bounds;
 import org.openstreetmap.josm.data.DataSource;
 import org.openstreetmap.josm.data.conflict.Conflict;
 import org.openstreetmap.josm.data.osm.DataSet;
+import org.openstreetmap.josm.data.osm.IPrimitive;
+import org.openstreetmap.josm.data.osm.OsmData;
 import org.openstreetmap.josm.data.osm.OsmPrimitive;
 import org.openstreetmap.josm.data.osm.visitor.BoundingXYVisitor;
 import org.openstreetmap.josm.data.osm.visitor.PrimitiveVisitor;
@@ -76,11 +78,11 @@ public class AutoScaleAction extends JosmAction {
      *
      */
     public static void zoomToSelection() {
-        DataSet dataSet = MainApplication.getLayerManager().getActiveDataSet();
+        OsmData<?, ?, ?, ?> dataSet = MainApplication.getLayerManager().getActiveData();
         if (dataSet == null) {
             return;
         }
-        Collection<OsmPrimitive> sel = dataSet.getSelected();
+        Collection<? extends IPrimitive> sel = dataSet.getSelected();
         if (sel.isEmpty()) {
             JOptionPane.showMessageDialog(
                     Main.parent,
@@ -96,7 +98,7 @@ public class AutoScaleAction extends JosmAction {
      * Zooms the view to display the given set of primitives.
      * @param sel The primitives to zoom to, e.g. the current selection.
      */
-    public static void zoomTo(Collection<OsmPrimitive> sel) {
+    public static void zoomTo(Collection<? extends IPrimitive> sel) {
         BoundingXYVisitor bboxCalculator = new BoundingXYVisitor();
         bboxCalculator.computeBoundingBox(sel);
         // increase bbox. This is required
@@ -353,7 +355,7 @@ public class AutoScaleAction extends JosmAction {
 
     @Override
     protected void updateEnabledState() {
-        DataSet ds = getLayerManager().getActiveDataSet();
+        OsmData<?, ?, ?, ?> ds = getLayerManager().getActiveData();
         MapFrame map = MainApplication.getMap();
         switch (mode) {
         case "selection":

src/org/openstreetmap/josm/actions/HistoryInfoAction.java
@@ -8,7 +8,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
 
 import org.openstreetmap.josm.Main;
-import org.openstreetmap.josm.data.osm.DataSet;
+import org.openstreetmap.josm.data.osm.OsmData;
 import org.openstreetmap.josm.gui.MainApplication;
 import org.openstreetmap.josm.gui.dialogs.OsmIdSelectionDialog;
 import org.openstreetmap.josm.gui.history.HistoryBrowserDialogManager;
@@ -37,7 +37,7 @@ public class HistoryInfoAction extends JosmAction {
 
     @Override
     public void actionPerformed(ActionEvent ae) {
-        DataSet set = getLayerManager().getActiveDataSet();
+        OsmData<?, ?, ?, ?> set = getLayerManager().getActiveData();
         if (set != null && !set.selectionEmpty()) {
             HistoryBrowserDialogManager.getInstance().showHistory(set.getAllSelected());
         } else {

src/org/openstreetmap/josm/actions/InfoAction.java
@@ -8,7 +8,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
 import java.util.Collection;
 
-import org.openstreetmap.josm.data.osm.DataSet;
+import org.openstreetmap.josm.data.osm.OsmData;
 import org.openstreetmap.josm.data.osm.OsmPrimitive;
 import org.openstreetmap.josm.gui.dialogs.InspectPrimitiveDialog;
 import org.openstreetmap.josm.tools.Shortcut;
@@ -33,7 +33,7 @@ public class InfoAction extends JosmAction {
 
     @Override
     public void actionPerformed(ActionEvent ae) {
-        DataSet set = getLayerManager().getActiveDataSet();
+        OsmData<?, ?, ?, ?> set = getLayerManager().getActiveData();
         if (set != null) {
             new InspectPrimitiveDialog(set.getAllSelected(), set).showDialog();
         }

src/org/openstreetmap/josm/actions/SelectAllAction.java
@@ -7,8 +7,8 @@ import static org.openstreetmap.josm.tools.I18n.tr;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
 
-import org.openstreetmap.josm.data.osm.DataSet;
-import org.openstreetmap.josm.data.osm.OsmPrimitive;
+import org.openstreetmap.josm.data.osm.IPrimitive;
+import org.openstreetmap.josm.data.osm.OsmData;
 import org.openstreetmap.josm.tools.Shortcut;
 
 /**
@@ -29,8 +29,8 @@ public class SelectAllAction extends JosmAction {
     public void actionPerformed(ActionEvent e) {
         if (!isEnabled())
             return;
-        DataSet ds = getLayerManager().getActiveDataSet();
-        ds.setSelected(ds.getPrimitives(OsmPrimitive::isSelectable));
+        OsmData<?, ?, ?, ?> ds = getLayerManager().getActiveData();
+        ds.setSelected(ds.getPrimitives(IPrimitive::isSelectable));
     }
 
     /**
@@ -38,6 +38,6 @@ public class SelectAllAction extends JosmAction {
      */
     @Override
     protected void updateEnabledState() {
-        setEnabled(getLayerManager().getActiveDataSet() != null);
+        setEnabled(getLayerManager().getActiveData() != null);
     }
 }

commit 170340f62761bcffce5b510f8a921670f0413c49

fix #16615 - make sure dataset can be garbage collected after removal (regression from r13926)
git-svn-id: https://josm.openstreetmap.de/svn/trunk@14156 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/actions/mapmode/SelectAction.java
@@ -221,6 +221,8 @@ public class SelectAction extends MapMode implements ModifierExListener, KeyPres
     @Override
     public void exitMode() {
         super.exitMode();
+        cycleManager.cycleStart = null;
+        cycleManager.cycleList = asColl(null);
         selectionManager.unregister(mv);
         mv.removeMouseListener(this);
         mv.removeMouseMotionListener(this);

src/org/openstreetmap/josm/gui/layer/MainLayerManager.java
@@ -250,7 +250,9 @@ public class MainLayerManager extends LayerManager {
         ActiveLayerChangeEvent event = new ActiveLayerChangeEvent(this, osmDataLayer, activeLayer);
         activeLayer = layer;
         if (activeLayer instanceof AbstractOsmDataLayer) {
-            dataLayer = (AbstractOsmDataLayer) layer;
+            dataLayer = (AbstractOsmDataLayer) activeLayer;
+        } else if (forceEditLayerUpdate) {
+            dataLayer = null;
         }
         if (activeLayer instanceof OsmDataLayer) {
             osmDataLayer = (OsmDataLayer) activeLayer;