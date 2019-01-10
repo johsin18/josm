johsin18/josm

commit b87ee5e35687fafa6e7f9ace6c34c9181dec7881

fix #8939 - prevent add/edit tag dialog to be created twice when hitting simultaneously enter key and button click
git-svn-id: https://josm.openstreetmap.de/svn/trunk@13854 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/gui/dialogs/properties/PropertiesDialog.java
@@ -1079,9 +1079,16 @@ implements SelectionChangedListener, ActiveLayerChangeListener, DataSetListenerA
         }
 
         @Override
-        public void actionPerformed(ActionEvent e) {
-            editHelper.addTag();
-            btnAdd.requestFocusInWindow();
+        public synchronized void actionPerformed(ActionEvent e) {
+            if (!isEnabled())
+                return;
+            setEnabled(false);
+            try {
+                editHelper.addTag();
+                btnAdd.requestFocusInWindow();
+            } finally {
+                setEnabled(true);
+            }
         }
     }
 
@@ -1097,15 +1104,20 @@ implements SelectionChangedListener, ActiveLayerChangeListener, DataSetListenerA
         }
 
         @Override
-        public void actionPerformed(ActionEvent e) {
+        public synchronized void actionPerformed(ActionEvent e) {
             if (!isEnabled())
                 return;
-            if (tagTable.getSelectedRowCount() == 1) {
-                int row = tagTable.getSelectedRow();
-                editHelper.editTag(row, false);
-            } else if (membershipTable.getSelectedRowCount() == 1) {
-                int row = membershipTable.getSelectedRow();
-                editMembership(row);
+            setEnabled(false);
+            try {
+                if (tagTable.getSelectedRowCount() == 1) {
+                    int row = tagTable.getSelectedRow();
+                    editHelper.editTag(row, false);
+                } else if (membershipTable.getSelectedRowCount() == 1) {
+                    int row = membershipTable.getSelectedRow();
+                    editMembership(row);
+                }
+            } finally {
+                setEnabled(true);
             }
         }
 

commit 329d50de09a61c412e98c3dce1205986fbb4520f

see #13160: Avoid additional deadlocks caused by r13854 (13160-v2.patch)
This probably doesn't fix #13160 but it seems to be a good step forward.



git-svn-id: https://josm.openstreetmap.de/svn/trunk@14669 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/gui/dialogs/properties/PropertiesDialog.java
@@ -26,6 +26,7 @@ import java.util.Optional;
 import java.util.Set;
 import java.util.TreeMap;
 import java.util.TreeSet;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import javax.swing.AbstractAction;
 import javax.swing.JComponent;
@@ -1097,6 +1098,7 @@ implements DataSelectionListener, ActiveLayerChangeListener, DataSetListenerAdap
      * Action handling add button press in properties dialog.
      */
     class AddAction extends JosmAction {
+        AtomicBoolean isPerforming = new AtomicBoolean(false);
         AddAction() {
             super(tr("Add"), /* ICON() */ "dialogs/add", tr("Add a new key/value pair to all objects"),
                     Shortcut.registerShortcut("properties:add", tr("Add Tag"), KeyEvent.VK_A,
@@ -1104,15 +1106,15 @@ implements DataSelectionListener, ActiveLayerChangeListener, DataSetListenerAdap
         }
 
         @Override
-        public synchronized void actionPerformed(ActionEvent e) {
-            if (!isEnabled())
+        public void actionPerformed(ActionEvent e) {
+            if (isPerforming.get())
                 return;
-            setEnabled(false);
+            isPerforming.set(true);
             try {
                 editHelper.addTag();
                 btnAdd.requestFocusInWindow();
             } finally {
-                setEnabled(true);
+                isPerforming.set(false);
             }
         }
     }
@@ -1121,6 +1123,7 @@ implements DataSelectionListener, ActiveLayerChangeListener, DataSetListenerAdap
      * Action handling edit button press in properties dialog.
      */
     class EditAction extends JosmAction implements ListSelectionListener {
+        AtomicBoolean isPerforming = new AtomicBoolean(false);
         EditAction() {
             super(tr("Edit"), /* ICON() */ "dialogs/edit", tr("Edit the value of the selected key for all objects"),
                     Shortcut.registerShortcut("properties:edit", tr("Edit Tags"), KeyEvent.VK_S,
@@ -1129,10 +1132,10 @@ implements DataSelectionListener, ActiveLayerChangeListener, DataSetListenerAdap
         }
 
         @Override
-        public synchronized void actionPerformed(ActionEvent e) {
-            if (!isEnabled())
+        public void actionPerformed(ActionEvent e) {
+            if (isPerforming.get())
                 return;
-            setEnabled(false);
+            isPerforming.set(true);
             try {
                 if (tagTable.getSelectedRowCount() == 1) {
                     int row = tagTable.getSelectedRow();
@@ -1142,7 +1145,7 @@ implements DataSelectionListener, ActiveLayerChangeListener, DataSetListenerAdap
                     editMembership(row);
                 }
             } finally {
-                setEnabled(true);
+                isPerforming.set(false);
             }
         }
 