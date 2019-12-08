johsin18/josm

commit a64f307a811ee6b72535c4f037e26e529f73a0a6

spotbugs - JLM_JSR166_UTILCONCURRENT_MONITORENTER
git-svn-id: https://josm.openstreetmap.de/svn/trunk@15525 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/data/cache/JCSCachedTileLoaderJob.java
@@ -152,15 +152,10 @@ public abstract class JCSCachedTileLoaderJob<K, V extends CacheEntry> implements
             Logging.warn("No url returned for: {0}, skipping", getCacheKey());
             throw new IllegalArgumentException("No url returned");
         }
-        synchronized (inProgress) {
-            Set<ICachedLoaderListener> newListeners = inProgress.get(deduplicationKey);
-            if (newListeners == null) {
-                newListeners = new HashSet<>();
-                inProgress.put(deduplicationKey, newListeners);
-                first = true;
-            }
-            newListeners.add(listener);
+        synchronized (this) {
+            first = !inProgress.containsKey(deduplicationKey);
         }
+        inProgress.computeIfAbsent(deduplicationKey, k -> new HashSet<>()).add(listener);
 
         if (first || force) {
             // submit all jobs to separate thread, so calling thread is not blocked with IO when loading from disk
@@ -249,13 +244,11 @@ public abstract class JCSCachedTileLoaderJob<K, V extends CacheEntry> implements
 
     private void finishLoading(LoadResult result) {
         Set<ICachedLoaderListener> listeners;
-        synchronized (inProgress) {
-            try {
-                listeners = inProgress.remove(getUrl().toString());
-            } catch (IOException e) {
-                listeners = null;
-                Logging.trace(e);
-            }
+        try {
+            listeners = inProgress.remove(getUrl().toString());
+        } catch (IOException e) {
+            listeners = null;
+            Logging.trace(e);
         }
         if (listeners == null) {
             Logging.warn("Listener not found for URL: {0}. Listener not notified!", getUrlNoException());

src/org/openstreetmap/josm/data/cache/HostLimitQueue.java
@@ -129,9 +129,7 @@ public class HostLimitQueue extends LinkedBlockingDeque<Runnable> {
         }
         Semaphore limit = hostSemaphores.get(host);
         if (limit == null) {
-            synchronized (hostSemaphores) {
-                limit = hostSemaphores.computeIfAbsent(host, k -> new Semaphore(hostLimit));
-            }
+            limit = hostSemaphores.computeIfAbsent(host, k -> new Semaphore(hostLimit));
         }
         return limit;
     }

src/org/openstreetmap/josm/data/imagery/TMSCachedTileLoaderJob.java
@@ -75,10 +75,7 @@ public class TMSCachedTileLoaderJob extends JCSCachedTileLoaderJob<String, Buffe
         this.tile = tile;
         this.options = options;
         if (listener != null) {
-            String deduplicationKey = getCacheKey();
-            synchronized (inProgress) {
-                inProgress.computeIfAbsent(deduplicationKey, k -> new HashSet<>()).add(listener);
-            }
+            inProgress.computeIfAbsent(getCacheKey(), k -> new HashSet<>()).add(listener);
         }
     }
 
@@ -161,10 +158,7 @@ public class TMSCachedTileLoaderJob extends JCSCachedTileLoaderJob<String, Buffe
     @Override
     public void loadingFinished(CacheEntry object, CacheEntryAttributes attributes, LoadResult result) {
         this.attributes = attributes; // as we might get notification from other object than our selfs, pass attributes along
-        Set<TileLoaderListener> listeners;
-        synchronized (inProgress) {
-            listeners = inProgress.remove(getCacheKey());
-        }
+        Set<TileLoaderListener> listeners = inProgress.remove(getCacheKey());
         boolean status = result == LoadResult.SUCCESS;
 
         try {

commit 12f4b552b6d843e5c7eda768ec951b05051fd322

fix #18404 - fix ConcurrentModificationException (regression from r15525)
git-svn-id: https://josm.openstreetmap.de/svn/trunk@15573 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/data/cache/JCSCachedTileLoaderJob.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.security.SecureRandom;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -155,7 +154,7 @@ public abstract class JCSCachedTileLoaderJob<K, V extends CacheEntry> implements
         synchronized (this) {
             first = !inProgress.containsKey(deduplicationKey);
         }
-        inProgress.computeIfAbsent(deduplicationKey, k -> new HashSet<>()).add(listener);
+        inProgress.computeIfAbsent(deduplicationKey, k -> ConcurrentHashMap.newKeySet()).add(listener);
 
         if (first || force) {
             // submit all jobs to separate thread, so calling thread is not blocked with IO when loading from disk