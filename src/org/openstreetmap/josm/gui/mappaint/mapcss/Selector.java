johsin18/josm

commit 2f6bcac4eeb2515f1fba9681f84ecc025a1e3eb3

see #18749 - Use efficient/unmodifiable collections
git-svn-id: https://josm.openstreetmap.de/svn/trunk@15909 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/gui/mappaint/mapcss/Selector.java
@@ -536,11 +536,7 @@ public interface Selector {
         protected final List<Condition> conds;
 
         protected AbstractSelector(List<Condition> conditions) {
-            if (conditions == null || conditions.isEmpty()) {
-                this.conds = null;
-            } else {
-                this.conds = conditions;
-            }
+            this.conds = Utils.toUnmodifiableList(conditions);
         }
 
         /**
@@ -551,7 +547,6 @@ public interface Selector {
         @Override
         public boolean matches(Environment env) {
             CheckParameterUtil.ensureParameterNotNull(env, "env");
-            if (conds == null) return true;
             for (Condition c : conds) {
                 try {
                     if (!c.applies(env)) return false;
@@ -568,10 +563,7 @@ public interface Selector {
          * @return the list of conditions
          */
         public List<Condition> getConditions() {
-            if (conds == null) {
-                return Collections.emptyList();
-            }
-            return Collections.unmodifiableList(conds);
+            return conds;
         }
     }
 

src/org/openstreetmap/josm/gui/mappaint/mapcss/MapCSSRule.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 
 import org.openstreetmap.josm.gui.mappaint.Environment;
 import org.openstreetmap.josm.gui.mappaint.StyleSource;
+import org.openstreetmap.josm.tools.Utils;
 
 /**
  * A MapCSS rule.
@@ -47,7 +48,7 @@ public class MapCSSRule implements Comparable<MapCSSRule> {
          * @param idx The index in the {@link StyleSource}
          */
         public Declaration(List<Instruction> instructions, int idx) {
-            this.instructions = instructions;
+            this.instructions = Utils.toUnmodifiableList(instructions);
             this.idx = idx;
         }
 

src/org/openstreetmap/josm/gui/tagging/presets/TaggingPresetSelector.java
@@ -88,12 +88,15 @@ public class TaggingPresetSelector extends SearchTextResultListPanel<TaggingPres
         public final TaggingPreset preset;
         public int classification;
         public int favoriteIndex;
-        private final Collection<String> groups = new HashSet<>();
-        private final Collection<String> names = new HashSet<>();
-        private final Collection<String> tags = new HashSet<>();
+        private final Collection<String> groups;
+        private final Collection<String> names;
+        private final Collection<String> tags;
 
         PresetClassification(TaggingPreset preset) {
             this.preset = preset;
+            Set<String> groups = new HashSet<>();
+            Set<String> names = new HashSet<>();
+            Set<String> tags = new HashSet<>();
             TaggingPreset group = preset.group;
             while (group != null) {
                 addLocaleNames(groups, group);
@@ -118,6 +121,9 @@ public class TaggingPresetSelector extends SearchTextResultListPanel<TaggingPres
                     }
                 }
             }
+            this.groups = Utils.toUnmodifiableList(groups);
+            this.names = Utils.toUnmodifiableList(names);
+            this.tags = Utils.toUnmodifiableList(tags);
         }
 
         private static void addLocaleNames(Collection<String> collection, TaggingPreset preset) {

src/org/openstreetmap/josm/tools/Utils.java
@@ -747,6 +747,27 @@ public final class Utils {
     }
 
     /**
+     * Returns an unmodifiable list for the given collection.
+     * Makes use of {@link Collections#emptySet()} and {@link Collections#singleton} and {@link Arrays#asList} to save memory.
+     * @param  collection the collection for which an unmodifiable collection is to be returned
+     * @param <T> the class of the objects in the array
+     * @return an unmodifiable list
+     * @see <a href="https://dzone.com/articles/preventing-your-java-collections-from-wasting-memo">
+     *     How to Prevent Your Java Collections From Wasting Memory</a>
+     */
+    @SuppressWarnings("unchecked")
+    public static <T> List<T> toUnmodifiableList(Collection<T> collection) {
+        // Java 9: use List.of(...)
+        if (collection == null || collection.isEmpty()) {
+            return Collections.emptyList();
+        } else if (collection.size() == 1) {
+            return Collections.singletonList(collection.iterator().next());
+        } else {
+            return (List<T>) Arrays.asList(collection.toArray());
+        }
+    }
+
+    /**
      * Returns the first not empty string in the given candidates, otherwise the default string.
      * @param defaultString default string returned if all candidates would be empty if stripped
      * @param candidates string candidates to consider

test/unit/org/openstreetmap/josm/tools/UtilsTest.java
@@ -4,13 +4,16 @@ package org.openstreetmap.josm.tools;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
 
 import java.awt.Color;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Locale;
 import java.util.regex.Pattern;
@@ -555,5 +558,19 @@ public class UtilsTest {
         assertFalse(Utils.hasExtension("JOSM.txt", ".jpg", ".txt"));
         assertTrue(Utils.hasExtension("JOSM.txt", "jpg", "txt"));
         assertTrue(Utils.hasExtension(new File("JOSM.txt"), "jpg", "txt"));
+    }
+
+    /**
+     * Test of {@link Utils#toUnmodifiableList}
+     */
+    @Test
+    public void testToUnmodifiableList() {
+        assertSame(Collections.emptyList(), Utils.toUnmodifiableList(null));
+        assertSame(Collections.emptyList(), Utils.toUnmodifiableList(Collections.emptyList()));
+        assertSame(Collections.emptyList(), Utils.toUnmodifiableList(new ArrayList<>()));
+        assertEquals(Collections.singletonList("foo"), Utils.toUnmodifiableList(new ArrayList<>(Collections.singletonList("foo"))));
+        assertEquals(Arrays.asList("foo", "bar", "baz"), Utils.toUnmodifiableList(Arrays.asList("foo", "bar", "baz")));
+        assertEquals(Arrays.asList("foo", "bar", "baz"), Utils.toUnmodifiableList(new ArrayList<>(Arrays.asList("foo", "bar", "baz"))));
+        assertEquals(Arrays.asList("foo", "bar", "baz"), Utils.toUnmodifiableList(new LinkedList<>(Arrays.asList("foo", "bar", "baz"))));
     }
 }

commit 10a65c30a978f5e1eb3461861e5dad408f66127c

see #18749: fix performance regression introduced with r15909
git-svn-id: https://josm.openstreetmap.de/svn/trunk@16191 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/gui/mappaint/mapcss/Selector.java
@@ -213,7 +213,7 @@ public interface Selector {
                 if (!left.matches(e.withPrimitive(parent)))
                     return;
                 int count = counter.getAsInt();
-                if (link.conds == null) {
+                if (link.conds == null || link.conds.isEmpty()) {
                     // index is not needed, we can avoid the sequential search below
                     e.parent = parent;
                     e.count = count;