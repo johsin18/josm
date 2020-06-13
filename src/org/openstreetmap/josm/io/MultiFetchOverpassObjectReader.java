johsin18/josm

commit 87e7d3befb6ec5d405d66dbcd414a78c31dc2ed8

fix #12303: When downloading objects with Overpass API, use recurse up to fetch referrers - new static method genOverpassQuery() to generate a single overpass query for all wanted objects - use POST instead of PUT to send the query - add handling for missing primitives (Overpass doesn't return invisible objects and doesn't a rc 404)
git-svn-id: https://josm.openstreetmap.de/svn/trunk@16611 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/io/MultiFetchOverpassObjectReader.java
@@ -1,11 +1,17 @@
 // License: GPL. For details, see LICENSE file.
 package org.openstreetmap.josm.io;
 
-import java.util.Map.Entry;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.TreeSet;
 import java.util.stream.Collectors;
 
 import org.openstreetmap.josm.data.osm.OsmPrimitiveType;
+import org.openstreetmap.josm.data.osm.PrimitiveId;
 import org.openstreetmap.josm.tools.Logging;
 
 /**
@@ -14,49 +20,101 @@ import org.openstreetmap.josm.tools.Logging;
  * @since 9241
  */
 public class MultiFetchOverpassObjectReader extends MultiFetchServerObjectReader {
+    private static final List<OsmPrimitiveType> wantedOrder = Arrays.asList(OsmPrimitiveType.RELATION,
+            OsmPrimitiveType.WAY, OsmPrimitiveType.NODE);
 
     private static String getPackageString(final OsmPrimitiveType type, Set<Long> idPackage) {
         return idPackage.stream().map(String::valueOf)
-                .collect(Collectors.joining(",", type.getAPIName() + (idPackage.size() == 1 ? "(" : "(id:"), ");"));
+                .collect(Collectors.joining(",", type.getAPIName() + (idPackage.size() == 1 ? "(" : "(id:"), ")"));
     }
 
     /**
-     * Create a single query for all elements
-     * @return the request string
+     * Generate single overpass query to retrieve multiple primitives. Can be used to download parents,
+     * children, the objects, or any combination of them.
+     * @param ids the collection of ids
+     * @param includeObjects if false, don't retrieve the primitives (e.g. only the referrers)
+     * @param recurseUp if true, referrers (parents) of the objects are downloaded and all nodes of parent ways
+     * @param recurseDownRelations true: yes, recurse down to retrieve complete relations
+     * @return the overpass query
+     * @since xxx
      */
-    protected String buildComplexRequestString() {
-        StringBuilder sb = new StringBuilder();
-        int countTypes = 0;
-        for (Entry<OsmPrimitiveType, Set<Long>> e : primitivesMap.entrySet()) {
-            if (!e.getValue().isEmpty()) {
-                countTypes++;
-                String list = getPackageString(e.getKey(), e.getValue());
-                switch (e.getKey()) {
-                case MULTIPOLYGON:
-                case RELATION:
-                    if (recurseDownRelations) {
-                        sb.append('(').append(list);
-                        sb.setLength(sb.length()-1); // remove semicolon
-                        //recurse down only one level, see #18835
-                        sb.append("->.r;.r;rel(r);.r;way(r);>;.r;node(r););");
-                    } else {
-                        sb.append(list);
+    public static String genOverpassQuery(Collection<? extends PrimitiveId> ids, boolean includeObjects, boolean recurseUp,
+            boolean recurseDownRelations) {
+        Map<OsmPrimitiveType, Set<Long>> primitivesMap = new LinkedHashMap<>();
+        Arrays.asList(OsmPrimitiveType.RELATION, OsmPrimitiveType.WAY, OsmPrimitiveType.NODE)
+                .forEach(type -> primitivesMap.put(type, new TreeSet<>()));
+        for (PrimitiveId p : ids) {
+            primitivesMap.get(p.getType()).add(p.getUniqueId());
+        }
+        return genOverpassQuery(primitivesMap, includeObjects, recurseUp, recurseDownRelations);
+    }
+
+    /**
+     * Generate single overpass query to retrieve multiple primitives. Can be used to download parents,
+     * children, the objects, or any combination of them.
+     * @param primitivesMap map containing the primitives
+     * @param includeObjects if false, don't retrieve the primitives (e.g. only the referrers)
+     * @param recurseUp if true, referrers (parents) of the objects are downloaded and all nodes of parent ways
+     * @param recurseDownRelations true: yes, recurse down to retrieve complete relations
+     * @return the overpass query
+     */
+    protected static String genOverpassQuery(Map<OsmPrimitiveType, Set<Long>> primitivesMap, boolean includeObjects,
+            boolean recurseUp, boolean recurseDownRelations) {
+        if (!(includeObjects || recurseUp || recurseDownRelations))
+            throw new IllegalArgumentException("At least one options must be true");
+        StringBuilder sb = new StringBuilder(128);
+        StringBuilder setsToInclude = new StringBuilder();
+        StringBuilder up = new StringBuilder();
+        String down = null;
+        for (OsmPrimitiveType type : wantedOrder) {
+            Set<Long> set = primitivesMap.get(type);
+            if (!set.isEmpty()) {
+                sb.append(getPackageString(type, set));
+                if (type == OsmPrimitiveType.NODE) {
+                    sb.append("->.n;");
+                    if (includeObjects) {
+                        setsToInclude.append(".n;");
                     }
-                    break;
-                case CLOSEDWAY:
-                case WAY:
-                    sb.append('(').append(list).append(">;);");
-                    break;
-                case NODE:
-                    sb.append(list);
+                    if (recurseUp) {
+                        up.append(".n;way(bn)->.wn;.n;rel(bn)->.rn;");
+                        setsToInclude.append(".wn;node(w);.rn;");
+                    }
+                } else if (type == OsmPrimitiveType.WAY) {
+                    sb.append("->.w;");
+                    if (includeObjects) {
+                        setsToInclude.append(".w;>;");
+                    }
+                    if (recurseUp) {
+                        up.append(".w;rel(bw)->.pw;");
+                        setsToInclude.append(".pw;");
+                    }
+                } else {
+                    sb.append("->.r;");
+                    if (includeObjects) {
+                        setsToInclude.append(".r;");
+                    }
+                    if (recurseUp) {
+                        up.append(".r;rel(br)->.pr;");
+                        setsToInclude.append(".pr;");
+                    }
+                    if (recurseDownRelations) {
+                        // get complete ways and nodes of the relation and next level of sub relations
+                        down = ".r;rel(r)->.rm;";
+                        setsToInclude.append(".r;>;.rm;");
+                    }
                 }
             }
         }
-        String query = sb.toString();
-        if (countTypes > 1) {
-            query = "(" + query + ");";
+        if (up.length() > 0) {
+            sb.append(up);
         }
-        query += "out meta;";
+        if (down != null) {
+            sb.append(down);
+        }
+        sb.append('(').append(setsToInclude).append(");");
+
+        sb.append("out meta;");
+        String query = sb.toString();
         Logging.debug("{0} {1}", "Generated Overpass query:", query);
         return query;
     }

test/unit/org/openstreetmap/josm/io/MultiFetchOverpassObjectReaderTest.java
@@ -4,10 +4,12 @@ package org.openstreetmap.josm.io;
 import static org.junit.Assert.assertEquals;
 
 import java.util.Arrays;
+import java.util.List;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.openstreetmap.josm.data.osm.Node;
+import org.openstreetmap.josm.data.osm.OsmPrimitive;
 import org.openstreetmap.josm.data.osm.Relation;
 import org.openstreetmap.josm.data.osm.Way;
 import org.openstreetmap.josm.testutils.JOSMTestRules;
@@ -30,11 +32,53 @@ public class MultiFetchOverpassObjectReaderTest {
      * Test {@link MultiFetchOverpassObjectReader#buildRequestString}
      */
     @Test
+    public void testBuildRequestNodesString() {
+        List<OsmPrimitive> objects = Arrays.asList(new Node(123), new Node(126), new Node(130));
+        String requestString;
+        // nodes without parents
+        requestString = MultiFetchOverpassObjectReader.genOverpassQuery(objects, true, false, false);
+        assertEquals("node(id:123,126,130)->.n;(.n;);out meta;", requestString);
+        requestString = MultiFetchOverpassObjectReader.genOverpassQuery(objects, true, false, true);
+        assertEquals("node(id:123,126,130)->.n;(.n;);out meta;", requestString);
+
+        // nodes with parents
+        requestString = MultiFetchOverpassObjectReader.genOverpassQuery(objects, true, true, false);
+        assertEquals("node(id:123,126,130)->.n;.n;way(bn)->.wn;.n;rel(bn)->.rn;(.n;.wn;node(w);.rn;);out meta;",
+                requestString);
+        requestString = MultiFetchOverpassObjectReader.genOverpassQuery(objects, true, true, true);
+        assertEquals("node(id:123,126,130)->.n;.n;way(bn)->.wn;.n;rel(bn)->.rn;(.n;.wn;node(w);.rn;);out meta;",
+                requestString);
+
+        // simulate download referrers
+        requestString = MultiFetchOverpassObjectReader.genOverpassQuery(objects, false, true, false);
+        assertEquals("node(id:123,126,130)->.n;.n;way(bn)->.wn;.n;rel(bn)->.rn;(.wn;node(w);.rn;);out meta;",
+                requestString);
+
+    }
+
+    /**
+     * Test {@link MultiFetchOverpassObjectReader#buildRequestString}
+     */
+    @Test
     public void testBuildRequestWaysString() {
-        MultiFetchOverpassObjectReader reader = new MultiFetchOverpassObjectReader();
-        reader.append(Arrays.asList(new Way(123), new Way(126), new Way(130)));
-        String requestString = reader.buildComplexRequestString();
-        assertEquals("(way(id:123,126,130);>;);out meta;", requestString);
+        List<OsmPrimitive> objects = Arrays.asList(new Way(123), new Way(126), new Way(130));
+        String requestString;
+        // ways without parents (always with nodes)
+        requestString = MultiFetchOverpassObjectReader.genOverpassQuery(objects, true, false, false);
+        assertEquals("way(id:123,126,130)->.w;(.w;>;);out meta;", requestString);
+        requestString = MultiFetchOverpassObjectReader.genOverpassQuery(objects, true, false, true);
+        assertEquals("way(id:123,126,130)->.w;(.w;>;);out meta;", requestString);
+
+        // ways with parents (always with nodes)
+        requestString = MultiFetchOverpassObjectReader.genOverpassQuery(objects, true, true, false);
+        assertEquals("way(id:123,126,130)->.w;.w;rel(bw)->.pw;(.w;>;.pw;);out meta;", requestString);
+        requestString = MultiFetchOverpassObjectReader.genOverpassQuery(objects, true, true, true);
+        assertEquals("way(id:123,126,130)->.w;.w;rel(bw)->.pw;(.w;>;.pw;);out meta;", requestString);
+
+        // simulate download referrers
+        requestString = MultiFetchOverpassObjectReader.genOverpassQuery(objects, false, true, false);
+        assertEquals("way(id:123,126,130)->.w;.w;rel(bw)->.pw;(.pw;);out meta;", requestString);
+
     }
 
     /**
@@ -42,14 +86,25 @@ public class MultiFetchOverpassObjectReaderTest {
      */
     @Test
     public void testBuildRequestRelationsString() {
-        MultiFetchOverpassObjectReader reader = new MultiFetchOverpassObjectReader();
-        reader.append(Arrays.asList(new Relation(123), new Relation(126), new Relation(130)));
-        reader.setRecurseDownRelations(true);
-        String requestString = reader.buildComplexRequestString();
-        assertEquals("(relation(id:123,126,130)->.r;.r;rel(r);.r;way(r);>;.r;node(r););out meta;", requestString);
-        reader.setRecurseDownRelations(false);
-        requestString = reader.buildComplexRequestString();
-        assertEquals("relation(id:123,126,130);out meta;", requestString);
+        List<OsmPrimitive> objects = Arrays.asList(new Relation(123), new Relation(126), new Relation(130));
+        String requestString;
+        // objects without parents or children
+        requestString = MultiFetchOverpassObjectReader.genOverpassQuery(objects, true, false, false);
+        assertEquals("relation(id:123,126,130)->.r;(.r;);out meta;", requestString);
+        // objects without parents, with children
+        requestString = MultiFetchOverpassObjectReader.genOverpassQuery(objects, true, false, true);
+        assertEquals("relation(id:123,126,130)->.r;.r;rel(r)->.rm;(.r;.r;>;.rm;);out meta;", requestString);
+        // objects with parents, without children
+        requestString = MultiFetchOverpassObjectReader.genOverpassQuery(objects, true, true, false);
+        assertEquals("relation(id:123,126,130)->.r;.r;rel(br)->.pr;(.r;.pr;);out meta;", requestString);
+        // objects with parents and with children
+        requestString = MultiFetchOverpassObjectReader.genOverpassQuery(objects, true, true, true);
+        assertEquals("relation(id:123,126,130)->.r;.r;rel(br)->.pr;.r;rel(r)->.rm;(.r;.pr;.r;>;.rm;);out meta;",
+                requestString);
+        // simulate download referrers
+        requestString = MultiFetchOverpassObjectReader.genOverpassQuery(objects, false, true, false);
+        assertEquals("relation(id:123,126,130)->.r;.r;rel(br)->.pr;(.pr;);out meta;", requestString);
+
     }
 
     /**
@@ -57,17 +112,33 @@ public class MultiFetchOverpassObjectReaderTest {
      */
     @Test
     public void testBuildComplexString() {
-        MultiFetchOverpassObjectReader reader = new MultiFetchOverpassObjectReader();
-        reader.setRecurseDownRelations(true);
-        reader.append(Arrays.asList(new Relation(123), new Relation(126), new Relation(130), new Way(88), new Way(99),
-                new Node(1)));
-        String requestString = reader.buildComplexRequestString();
-        assertEquals(
-                "((relation(id:123,126,130)->.r;.r;rel(r);.r;way(r);>;.r;node(r););(way(id:88,99);>;);node(1););out meta;",
-                requestString);
-        reader.setRecurseDownRelations(false);
-        requestString = reader.buildComplexRequestString();
-        assertEquals("(relation(id:123,126,130);(way(id:88,99);>;);node(1););out meta;", requestString);
-    }
+        List<OsmPrimitive> objects = Arrays.asList(new Relation(123), new Relation(126), new Relation(130), new Way(88), new Way(99),
+                new Node(1));
+        // all request strings should start with the same list of objects
+        final String ids =  "relation(id:123,126,130)->.r;way(id:88,99)->.w;node(1)->.n;";
+        String requestString;
 
+        // objects without parents (ways always with nodes)
+        requestString = MultiFetchOverpassObjectReader.genOverpassQuery(objects, true, false, false);
+        assertEquals(ids + "(.r;.w;>;.n;);out meta;", requestString);
+        // objects without parents (ways always with nodes), recurse down one level for sub relations
+        requestString = MultiFetchOverpassObjectReader.genOverpassQuery(objects, true, false, true);
+        assertEquals(ids + ".r;rel(r)->.rm;(.r;.r;>;.rm;.w;>;.n;);out meta;", requestString);
+
+        // objects with parents
+        requestString = MultiFetchOverpassObjectReader.genOverpassQuery(objects, true, true, false);
+        assertEquals(
+                ids + ".r;rel(br)->.pr;.w;rel(bw)->.pw;.n;way(bn)->.wn;.n;rel(bn)->.rn;(.r;.pr;.w;>;.pw;.n;.wn;node(w);.rn;);out meta;",
+                requestString);
+
+        // objects with parents, recurse down one level for sub relations
+        requestString = MultiFetchOverpassObjectReader.genOverpassQuery(objects, true, true, true);
+        assertEquals(ids + ".r;rel(br)->.pr;.w;rel(bw)->.pw;.n;way(bn)->.wn;.n;rel(bn)->.rn;.r;rel(r)->.rm;"
+                + "(.r;.pr;.r;>;.rm;.w;>;.pw;.n;.wn;node(w);.rn;);out meta;", requestString);
+        // simulate download referrers
+        requestString = MultiFetchOverpassObjectReader.genOverpassQuery(objects, false, true, false);
+        assertEquals(
+                ids + ".r;rel(br)->.pr;.w;rel(bw)->.pw;.n;way(bn)->.wn;.n;rel(bn)->.rn;(.pr;.pw;.wn;node(w);.rn;);out meta;",
+                requestString);
+    }
 }

src/org/openstreetmap/josm/actions/downloadtasks/DownloadReferrersTask.java
@@ -14,6 +14,7 @@ import java.util.stream.Collectors;
 import javax.swing.JOptionPane;
 import javax.swing.SwingUtilities;
 
+import org.openstreetmap.josm.data.Bounds;
 import org.openstreetmap.josm.data.osm.DataSet;
 import org.openstreetmap.josm.data.osm.DataSetMerger;
 import org.openstreetmap.josm.data.osm.Node;
@@ -26,10 +27,12 @@ import org.openstreetmap.josm.gui.MapFrame;
 import org.openstreetmap.josm.gui.PleaseWaitRunnable;
 import org.openstreetmap.josm.gui.layer.OsmDataLayer;
 import org.openstreetmap.josm.gui.progress.ProgressMonitor;
+import org.openstreetmap.josm.io.MultiFetchOverpassObjectReader;
 import org.openstreetmap.josm.io.MultiFetchServerObjectReader;
 import org.openstreetmap.josm.io.OsmServerBackreferenceReader;
 import org.openstreetmap.josm.io.OsmServerReader;
 import org.openstreetmap.josm.io.OsmTransferException;
+import org.openstreetmap.josm.io.OverpassDownloadReader;
 import org.openstreetmap.josm.tools.CheckParameterUtil;
 import org.openstreetmap.josm.tools.ExceptionUtil;
 import org.xml.sax.SAXException;
@@ -154,37 +157,45 @@ public class DownloadReferrersTask extends PleaseWaitRunnable {
     @Override
     protected void realRun() throws SAXException, IOException, OsmTransferException {
         try {
-            progressMonitor.setTicksCount(children.size());
-            int i = 1;
-            for (PrimitiveId p : children) {
-                if (canceled)
-                    return;
-                String msg;
-                String id = Long.toString(p.getUniqueId());
-                switch(p.getType()) {
-                case NODE: msg = tr("({0}/{1}) Loading parents of node {2}", i, children.size(), id); break;
-                case WAY: msg = tr("({0}/{1}) Loading parents of way {2}", i, children.size(), id); break;
-                case RELATION: msg = tr("({0}/{1}) Loading parents of relation {2}", i, children.size(), id); break;
-                default: throw new AssertionError();
-                }
-                progressMonitor.subTask(msg);
-                downloadParents(p.getUniqueId(), p.getType(), progressMonitor);
-                i++;
-            }
-            Collection<Way> ways = parents.getWays();
-
-            if (!ways.isEmpty()) {
-                // Collect incomplete nodes of parent ways
-                Set<Node> nodes = ways.stream().flatMap(w -> w.getNodes().stream().filter(OsmPrimitive::isIncomplete))
-                        .collect(Collectors.toSet());
-                if (!nodes.isEmpty()) {
-                    reader = MultiFetchServerObjectReader.create();
-                    ((MultiFetchServerObjectReader) reader).append(nodes);
-                    DataSet wayNodes = reader.parseOsm(progressMonitor.createSubTaskMonitor(1, false));
-                    synchronized (this) { // avoid race condition in cancel()
-                        reader = null;
+            if (Boolean.TRUE.equals(OverpassDownloadReader.FOR_MULTI_FETCH.get())) {
+                String request = MultiFetchOverpassObjectReader.genOverpassQuery(children, false, true, false);
+                reader = new OverpassDownloadReader(new Bounds(0, 0, 0, 0),
+                        OverpassDownloadReader.OVERPASS_SERVER.get(), request);
+                DataSet ds = reader.parseOsm(progressMonitor.createSubTaskMonitor(1, false));
+                new DataSetMerger(parents, ds).merge();
+            } else {
+                progressMonitor.setTicksCount(children.size());
+                int i = 1;
+                for (PrimitiveId p : children) {
+                    if (canceled)
+                        return;
+                    String msg;
+                    String id = Long.toString(p.getUniqueId());
+                    switch(p.getType()) {
+                    case NODE: msg = tr("({0}/{1}) Loading parents of node {2}", i, children.size(), id); break;
+                    case WAY: msg = tr("({0}/{1}) Loading parents of way {2}", i, children.size(), id); break;
+                    case RELATION: msg = tr("({0}/{1}) Loading parents of relation {2}", i, children.size(), id); break;
+                    default: throw new AssertionError();
                     }
-                    new DataSetMerger(parents, wayNodes).merge();
+                    progressMonitor.subTask(msg);
+                    downloadParents(p.getUniqueId(), p.getType(), progressMonitor);
+                    i++;
+                }
+                Collection<Way> ways = parents.getWays();
+
+                if (!ways.isEmpty()) {
+                    // Collect incomplete nodes of parent ways
+                    Set<Node> nodes = ways.stream().flatMap(w -> w.getNodes().stream().filter(OsmPrimitive::isIncomplete))
+                            .collect(Collectors.toSet());
+                    if (!nodes.isEmpty()) {
+                        reader = MultiFetchServerObjectReader.create();
+                        ((MultiFetchServerObjectReader) reader).append(nodes);
+                        DataSet wayNodes = reader.parseOsm(progressMonitor.createSubTaskMonitor(1, false));
+                        synchronized (this) { // avoid race condition in cancel()
+                            reader = null;
+                        }
+                        new DataSetMerger(parents, wayNodes).merge();
+                    }
                 }
             }
         } catch (OsmTransferException e) {
@@ -193,4 +204,5 @@ public class DownloadReferrersTask extends PleaseWaitRunnable {
             lastException = e;
         }
     }
+
 }

src/org/openstreetmap/josm/gui/io/DownloadFromOverpassTask.java
@@ -1,0 +1,70 @@
+// License: GPL. For details, see LICENSE file.
+package org.openstreetmap.josm.gui.io;
+
+import static org.openstreetmap.josm.tools.I18n.tr;
+
+import java.io.IOException;
+
+import org.openstreetmap.josm.data.Bounds;
+import org.openstreetmap.josm.data.osm.DataSet;
+import org.openstreetmap.josm.data.osm.DataSetMerger;
+import org.openstreetmap.josm.gui.ExceptionDialogUtil;
+import org.openstreetmap.josm.gui.PleaseWaitRunnable;
+import org.openstreetmap.josm.gui.progress.ProgressMonitor;
+import org.openstreetmap.josm.io.OsmTransferException;
+import org.openstreetmap.josm.io.OverpassDownloadReader;
+import org.xml.sax.SAXException;
+
+/**
+ * Download OSM data from Overpass API
+ *
+ */
+public class DownloadFromOverpassTask extends PleaseWaitRunnable {
+    private boolean canceled;
+    private final String request;
+    private final DataSet ds;
+    private Exception lastException;
+
+    /**
+     * Constructor
+     * @param request the overpass query
+     * @param ds the {@code DataSet} instance that should contain the downloaded data
+     * @param monitor ProgressMonitor to use or null to create a new one.
+     */
+    public DownloadFromOverpassTask(String request, DataSet ds, ProgressMonitor monitor) {
+        super(tr("Download objects via Overpass API"), monitor, false);
+        this.request = request;
+        this.ds = ds;
+    }
+
+    @Override
+    protected void cancel() {
+        canceled = true;
+    }
+
+    @Override
+    protected void realRun() throws SAXException, IOException, OsmTransferException {
+        try {
+            OverpassDownloadReader reader = new OverpassDownloadReader(new Bounds(0, 0, 0, 0),
+                    OverpassDownloadReader.OVERPASS_SERVER.get(), request);
+            DataSet tmpDs = reader.parseOsm(progressMonitor.createSubTaskMonitor(1, false));
+            if (!canceled) {
+                new DataSetMerger(ds, tmpDs).merge();
+            }
+        } catch (OsmTransferException e) {
+            if (canceled)
+                return;
+            lastException = e;
+        }
+
+    }
+
+    @Override
+    protected void finish() {
+        if (canceled)
+            return;
+        if (lastException != null) {
+            ExceptionDialogUtil.explainException(lastException);
+        }
+    }
+}

commit 13f5cb927b65713c9c4c056c61796571c3ed54c3

fix checkstyle, pmd issues introduced with r16611
git-svn-id: https://josm.openstreetmap.de/svn/trunk@16612 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/io/MultiFetchOverpassObjectReader.java
@@ -111,9 +111,8 @@ public class MultiFetchOverpassObjectReader extends MultiFetchServerObjectReader
         if (down != null) {
             sb.append(down);
         }
-        sb.append('(').append(setsToInclude).append(");");
+        sb.append('(').append(setsToInclude).append(");out meta;");
 
-        sb.append("out meta;");
         String query = sb.toString();
         Logging.debug("{0} {1}", "Generated Overpass query:", query);
         return query;

test/unit/org/openstreetmap/josm/io/MultiFetchOverpassObjectReaderTest.java
@@ -115,7 +115,7 @@ public class MultiFetchOverpassObjectReaderTest {
         List<OsmPrimitive> objects = Arrays.asList(new Relation(123), new Relation(126), new Relation(130), new Way(88), new Way(99),
                 new Node(1));
         // all request strings should start with the same list of objects
-        final String ids =  "relation(id:123,126,130)->.r;way(id:88,99)->.w;node(1)->.n;";
+        final String ids = "relation(id:123,126,130)->.r;way(id:88,99)->.w;node(1)->.n;";
         String requestString;
 
         // objects without parents (ways always with nodes)